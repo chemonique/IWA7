753115e229bffa9e56d0fb8f36cb386c0c9821e6
chemonique <122254404+chemonique@users.noreply.github.com>
2023-04-19 10:03:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,28 +14,7 @@ if (nickname) {
   console.log("Good morning!");
 }
 
-//challenge 3
-
-// const leoName = 'Leo'
-// const leoSurname = 'Musvaire     '
-// const leoBalance = '-9394'
-
-// const sarahName = 'Sarah    '
-// const sarahSurname = 'Kleinhans'
-// const sarahBalance = '-4582.21000111'
-
-// const divider = '----------------------------------'
-
-// // Only change below this line
-
-// const owed = parseFloat(leoBalance) + parseFloat(sarahBalance)
-// const leo = `${leoName} ${leoSurname} Owed: R ${parseFloat(leoBalance).toFixed(2)}\n`
-// const sarah = `${sarahName} ${sarahSurname} Owed: R ${parseFloat(sarahBalance).toFixed(2)}\n`
-// const total = `  Total amount owed: R ${owed.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}\n`
-// const result = `\n${leo}${sarah}${divider}\n${total}${divider}\n`
-
-// console.log(result)
-
+//challenge 3 
 const leoName = 'Leo'
 const leoSurname = 'Musvaire     '
 const leoBalance = '-9394'
@@ -48,10 +27,10 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = 'R '+ parseFloat((parseInt(leoBalance)+parseInt(sarahBalance)) *-1).toFixed(2)
-const leo = `${leoName}  ${leoSurname} (Owed: R ${parseFloat(leoBalance*-1).toFixed(2)})\n`
-const sarah = `${sarahName} ${sarahSurname} (Owed: R ${parseFloat(sarahBalance*-1).toFixed(2)})\n`
-const total = `Total amount owed: ${owed.replace(/\B(?=(\d{3})+(?!\d))/g, " ")} `
+const owed = (parseFloat(leoBalance) + parseFloat(sarahBalance))*-1
+const leo = `${leoName} ${leoSurname} (Owed: R ${parseFloat(leoBalance*-1).toFixed(2)})\n`
+const sarah = `${sarahName} ${sarahSurname} (Owed: R ${parseFloat(sarahBalance*-1).toFixed(2)})\n\n`
+const total = ` \nTotal amount owed: R ${owed.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ")}\n`
 const result = `\n${leo}${sarah}\n${divider}\n${total}\n${divider}\n`
 
 console.log(result)
